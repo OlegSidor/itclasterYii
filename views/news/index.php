b0478cb4df735a390e708eb572b43e198ceab4ed
oleh <mrx27898@gmail.com>
2018-05-12 14:37:04 +0300

    enable pretty url

--- views/news/index.php
+++ views/news/index.php
@@ -6,7 +6,7 @@ use yii\widgets\LinkPager;
 <h1>Новини</h1>
 <?php foreach ($rows as $row): ?>
   <div class="item">
-  <a href="index.php?r=news/one&id=<?= Html::encode($row->id) ?>"><?= Html::encode($row->title) ?></a>
+  <a href="one?id=<?= Html::encode($row->id) ?>"><?= Html::encode($row->title) ?></a>
         <p style="float:right;">Created by: <?= Html::encode($row->user->username) ?></p>
         <p>Category: <?= Html::encode($row->category->name) ?></p>
         <hr>
